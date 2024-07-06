13a35d68d42cd48ef2da199c5c7694c844f24342
MEGNIGBETO <souvenirmegnigbeto@gmail.com>
2024-07-07 01:24:24 +0200

    modif reussie: amelioration window.addEventListener

--- script2.js
+++ script2.js
@@ -1,26 +1,28 @@
-document.getElementById("demarrer").onclick = function() { 
-    function domReady(fn) {
-        if (
-            document.readyState === "complete" ||
-            document.readyState === "interactive"
-        ) {
-            setTimeout(fn, 1000);
-        } else {
-            document.addEventListener("DOMContentLoaded", fn);
+window.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("demarrer").onclick = function() { 
+        function domReady(fn) {
+            if (
+                document.readyState === "complete" ||
+                document.readyState === "interactive"
+            ) {
+                setTimeout(fn, 1000);
+            } else {
+                document.addEventListener("DOMContentLoaded", fn);
+            }
         }
-    }
-
-    // If found you qr code
-    function onScanSuccess(decodeText, decodeResult) {
-        alert("You Qr is : " + decodeText, decodeResult);
-    }
-
-    domReady(function () {
-
-        let htmlscanner = new Html5QrcodeScanner(
-            "my-qr-reader",
-            { fps: 10, qrbos: 250 }
-        );
-        htmlscanner.render(onScanSuccess);
-    });
-};
+    
+        // If found you qr code
+        function onScanSuccess(decodeText, decodeResult) {
+            alert("You Qr is : " + decodeText, decodeResult);
+        }
+    
+        domReady(function () {
+    
+            let htmlscanner = new Html5QrcodeScanner(
+                "my-qr-reader",
+                { fps: 10, qrbos: 250 }
+            );
+            htmlscanner.render(onScanSuccess);
+        });
+    };  
+});
